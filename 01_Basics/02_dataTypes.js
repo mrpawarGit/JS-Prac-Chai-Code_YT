18b4d363ef9e4dd19cdadf614eaeb597214a667e
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2024-07-03 15:10:24 +0000

    datatyes and conversions in js

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //trats all JS code as new version
+//alert(3+3); //not work beacuse we are running it in node js not js
+
+console.log(5+7+9+8);
+
+
+function name(params) {
+    console.log("Hello "+params);
+}
+
+name("Mayur");
+
+
+
+
---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score = 33;
+console.log(score);   
+
+console.log(typeof(score));
+console.log(typeof score);
+
+//checking datatype - can use both ways
+let score2 = "55";
+console.log(typeof(score2));
+console.log();
+
+
+//converting num to string
+let str = Number(score2);
+console.log(typeof(str));
+console.log(typeof(score2));
+console.log(str+2);
+console.log(score2+2);
+
+//Number(score2);
+//console.log(typeof(score2));
+
+
