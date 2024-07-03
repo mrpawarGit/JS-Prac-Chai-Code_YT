6ec429ee655a17daddf3e21ecb842dcea99e4bd0
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2024-07-03 14:28:39 +0000

    Done

---
+++ 01_Basics/variables.js
@@ -0,0 +1,20 @@
+const accountID = 123456;
+let accountEmail = "mayur@gmail.com";
+var accountPass = "12345";
+accountCity = "Pune";
+let accountState;
+
+console.log(accountID);
+
+/*
+accountID=223;
+const can't be modified
+*/
+
+/*
+- prfer not to use 'var'
+- beacause of issue in block scope and functional scope
+- use 'let' & 'const'
+*/
+
+console.table([accountID,accountEmail,accountPass,accountCity,accountState]);
